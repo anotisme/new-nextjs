24b66835b99b91970c924e38daed5a4212a132a3
A Not <a.not@enouvo.com>
2022-03-04 12:26:33 +0700

    Add API

--- config/index.js
+++ config/index.js
@@ -1,3 +1,2 @@
-const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000/' : 'https://new-nextjs-five.vercel.app/';
+export const server = 'http://localhost:3000' ?? 'https://new-nextjs-five.vercel.app';
--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,11 +1,11 @@
-export default API = (res) => {
+export default (req,res) => {
 	res.status(200).json({ 
 		posts: [
 			{title: "Post 1", content: "Content 1"},
 			{title: "Post 2", content: "Content 2"},
 			{title: "Post 3", content: "Content 3"},
 			{title: "Post 4", content: "Content 4"},
-			{title: "Post 5", content: "Content 6"}
+			{title: "Post 5", content: "Content 5"}
 		]
 	})
 }
